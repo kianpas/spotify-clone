78c7492992b74be0f73b86c690e6b539bf9de1e7
kianpas <kianpas@gmail.com>
2023-09-01 16:01:06 +0900

    23.09.01 supabase, stripe 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   );
